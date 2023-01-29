f1ccabc48dc8c385c3fe85b5752d3864f98badf8
Alex Guérin <alexguerin.pro@gmail.com>
2023-01-29 20:25:55 +0100

    Ajoutez des fichiers projet.

---
+++ Tasks/Project.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks
+{
+    internal class Project
+    {
+        public Dictionary<long, dynamic> _tasksArrays = new Dictionary<long, dynamic>();
+
+        public void Add(long taskIdentifier, string taskDescription, bool taskDone)
+        {
+            dynamic d1 = new System.Dynamic.ExpandoObject();
+            d1.description = taskDescription;
+            d1.done = taskDone;
+
+            _tasksArrays.Add(taskIdentifier, d1);
+        }
+
+        public void PrintInfo(IConsole console)
+        {
+            foreach (var task in _tasksArrays)
+            {
+                var taskKey = task.Key;
+                var taskValue = task.Value;
+                var taskDescription = taskValue.description;
+                var taskDone = taskValue.done;
+                
+                console.WriteLine($"    [{(taskDone ? 'x' : ' ')}] {taskKey}: {taskDescription}");
+            }
+        }
+
+        public void SetDoneIfExists(string identifier, bool done, IConsole console)
+        {
+            long parseIdentifier = long.Parse(identifier);
+            if (_tasksArrays.ContainsKey(parseIdentifier))
+            {
+                _tasksArrays[parseIdentifier].done = done;
+            }
+        }
+    }
+}
